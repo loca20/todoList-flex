a23af9d04036aab57685fd466462da4fc1bec1a9
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-09-03 15:26:56 +0200

    initial js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+{
+	const tasks = [
+		{
+			content: "wykonać zadanie",
+			done: false,
+		},
+		{
+			content: "zjeść obiad",
+			done: true,
+		},
+	];
+
+	const render = () => {
+		let htmlString = "";
+
+		for (const task of tasks) {
+			htmlString += `
+        <li>
+        ${task.content}
+        </li>
+        `;
+		}
+
+		document.querySelector(".js-tasks").innerHTML = htmlString;
+	};
+
+	const init = () => {
+		render();
+	};
+
+	init();
+}
